5251abdc6b5617169315693cf2370697284e5fc8
Jaime Alvarez <jaimealonso.alvarez1984@gmail.com>
2023-12-03 20:22:25 -0500

    minor changes

--- src/app/modules/shared/constants/constants.ts
+++ src/app/modules/shared/constants/constants.ts
@@ -1,4 +1,5 @@
 import { environment } from 'environments/environment'
 
 export const apiUrl = 
-  `${environment.api_server.protocol}://${environment.api_server.host}:${environment.api_server.port}/api`
+  // `${environment.api_server.protocol}://${environment.api_server.host}:${environment.api_server.port}/api`
+  'http://cea-api.onrender.com/api'
